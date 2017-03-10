e1beb4a9fe895bff837a7828cfbaec1f459c38a6
Shengwen Cheng <shengwen1997.tw@gmail.com>
2017-03-11 00:05:08 +0800

    Initial import

---
+++ program/main.c
@@ -0,0 +1,60 @@
+#include "stm32f7xx.h"
+
+void led_on(void)
+{
+	HAL_GPIO_WritePin(GPIOD, GPIO_PIN_4, GPIO_PIN_SET);
+	HAL_GPIO_WritePin(GPIOD, GPIO_PIN_5, GPIO_PIN_SET);
+	HAL_GPIO_WritePin(GPIOD, GPIO_PIN_6, GPIO_PIN_SET);
+}
+
+
+void led_off(void)
+{
+	HAL_GPIO_WritePin(GPIOD, GPIO_PIN_4, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(GPIOD, GPIO_PIN_5, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(GPIOD, GPIO_PIN_6, GPIO_PIN_RESET);
+}
+
+void gpio_init(void)
+{
+	__HAL_RCC_GPIOD_CLK_ENABLE();
+
+	GPIO_InitTypeDef gpio_init_struct = {
+		.Pin = GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_6,
+		.Mode = GPIO_MODE_OUTPUT_PP,
+		.Pull = GPIO_PULLUP,
+		.Speed = GPIO_SPEED_HIGH
+	};
+
+	HAL_GPIO_Init(GPIOD, &gpio_init_struct);
+
+	led_off();
+}
+
+void delay(uint32_t count)
+{
+        while(count--) {
+		__asm volatile("nop");
+	}
+}
+
+int main(void)
+{
+	gpio_init();
+
+	int state = 1;
+
+	while(1) {
+		if(state == 1) {
+			led_on();
+		} else {
+			led_off();
+		}
+
+		delay(1000000);
+
+		state = (state + 1) % 2;
+	}
+
+	return 0;
+}
