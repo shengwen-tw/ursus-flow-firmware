bb70ee87b46e7007f6b8b937b6eeb66170906cdd
Shengwen Cheng <shengwen1997.tw@gmail.com>
2017-03-18 16:36:57 +0800

    Import ST usb cdc

---
+++ program/driver/peripherial/usb/usbd_cdc_if.h
@@ -0,0 +1,18 @@
+#ifndef __USBD_CDC_IF_H
+#define __USBD_CDC_IF_H
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+#include "usbd_cdc.h"
+
+extern USBD_CDC_ItfTypeDef  USBD_Interface_fops_FS;
+
+uint8_t CDC_Transmit_FS(uint8_t* Buf, uint16_t Len);
+
+#ifdef __cplusplus
+}
+#endif
+  
+#endif
