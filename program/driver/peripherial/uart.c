e2c51a475a2d6599e2dc9226c136701ebeda892a
Shengwen Cheng <shengwen1997.tw@gmail.com>
2017-03-18 21:38:33 +0800

    Setting dma for uart2 (not workable yet)

--- program/driver/peripherial/uart.c
+++ program/driver/peripherial/uart.c
@@ -17,6 +17,7 @@ void uart2_init(int baudrate)
 
 	uart2.Instance = USART2;
 	uart2.Init.BaudRate = baudrate;
+	uart2.Init.WordLength = UART_WORDLENGTH_8B;
 	uart2.Init.StopBits = UART_STOPBITS_1;
 	uart2.Init.Parity = UART_PARITY_NONE;
 	uart2.Init.Mode = UART_MODE_TX_RX;
@@ -36,9 +37,26 @@ void uart2_init(int baudrate)
 	};
 
 	HAL_GPIO_Init(GPIOA, &gpio);
+
+	/* DMA1 channel4 stream6 for UART2 tx */
+	DMA_HandleTypeDef uart2_tx_dma = {
+		.Instance = DMA1_Stream6, 
+		.Init.Channel = DMA_CHANNEL_4,
+		.Init.Direction = DMA_MEMORY_TO_PERIPH,
+		.Init.PeriphInc = DMA_PINC_DISABLE,
+		.Init.MemInc = DMA_MINC_ENABLE,
+		.Init.PeriphDataAlignment = DMA_PDATAALIGN_BYTE,
+		.Init.MemDataAlignment = DMA_PDATAALIGN_BYTE,
+		.Init.Mode = DMA_NORMAL,
+		.Init.Priority = DMA_PRIORITY_LOW,
+	};
+
+	HAL_DMA_Init(&uart2_tx_dma);
+	__HAL_LINKDMA(&uart2, hdmatx, uart2_tx_dma);
 }
 
 void uart2_puts(char *str)
 {
+	//HAL_UART_Transmit_DMA(&uart2, (uint8_t*)str, strlen(str));
 	HAL_UART_Transmit(&uart2, (uint8_t*)str, strlen(str), 100);
 }
