c095186281b36794042306f23214df6e1d2453bb
Shengwen Cheng <shengwen1997.tw@gmail.com>
2017-03-27 15:28:53 +0800

    Replace timer8 with timer3, able to output pwm
    *Precision is very bad

--- program/driver/peripherial/pwm.c
+++ program/driver/peripherial/pwm.c
@@ -1,70 +1,57 @@
 #include "stm32f7xx.h"
 
-static void timer8_pwm_init(void);
+static void timer3_pwm_init(void);
 
-TIM_HandleTypeDef pwm_timer8;
+TIM_HandleTypeDef pwm_timer3;
 
 void pwm_init(void)
 {
-	timer8_pwm_init();
+	timer3_pwm_init();
 }
 
-static void timer8_pwm_init(void)
+static void timer3_pwm_init(void)
 {
-	__HAL_RCC_TIM8_CLK_ENABLE();
+	__HAL_RCC_TIM3_CLK_ENABLE();
+	__HAL_RCC_GPIOC_CLK_ENABLE();
 
-	pwm_timer8.Instance = TIM8;
-	pwm_timer8.Init.Prescaler = 1080 - 1;
-	pwm_timer8.Init.CounterMode = TIM_COUNTERMODE_UP;
-	pwm_timer8.Init.Period = 2000 - 1;
-	pwm_timer8.Init.ClockDivision = TIM_CLOCKDIVISION_DIV2;
-	pwm_timer8.Init.RepetitionCounter = 0;
+	GPIO_InitTypeDef pwm_gpio = {
+		.Pin = GPIO_PIN_7,
+		.Mode = GPIO_MODE_AF_PP,
+		.Pull = GPIO_NOPULL,
+		.Speed = GPIO_SPEED_FREQ_VERY_HIGH,
+		.Alternate = GPIO_AF2_TIM3
+	};
+	HAL_GPIO_Init(GPIOC, &pwm_gpio);
 
-	if(HAL_TIM_PWM_Init(&pwm_timer8) != HAL_OK) {
+	pwm_timer3.Instance = TIM3;
+	pwm_timer3.Init.Prescaler = 1 - 1;
+	pwm_timer3.Init.CounterMode = TIM_COUNTERMODE_UP;
+	pwm_timer3.Init.Period = 4 - 1;
+	pwm_timer3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+
+	if(HAL_TIM_PWM_Init(&pwm_timer3) != HAL_OK) {
 		//Error_Handler();
 	}
 
 	TIM_MasterConfigTypeDef pwm_master_config = {
 		.MasterOutputTrigger = TIM_TRGO_RESET,
-		.MasterOutputTrigger2 = TIM_TRGO2_RESET,
 		.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE
 	};
 
-	if(HAL_TIMEx_MasterConfigSynchronization(&pwm_timer8, &pwm_master_config) != HAL_OK) {
-		//Error_Handler();
-	}
-
-	TIM_BreakDeadTimeConfigTypeDef pwm_break_dead_config = {
-		.OffStateRunMode = TIM_OSSR_DISABLE,
-		.OffStateIDLEMode = TIM_OSSI_DISABLE,
-		.LockLevel = TIM_LOCKLEVEL_OFF,
-		.DeadTime = 0,
-		.BreakState = TIM_BREAK_DISABLE,
-		.BreakPolarity = TIM_BREAKPOLARITY_HIGH,
-		.BreakFilter = 0,
-		.Break2State = TIM_BREAK2_DISABLE,
-		.Break2Polarity = TIM_BREAK2POLARITY_HIGH,
-		.Break2Filter = 0,
-		.AutomaticOutput = TIM_AUTOMATICOUTPUT_DISABLE
-	};
-
-	if(HAL_TIMEx_ConfigBreakDeadTime(&pwm_timer8, &pwm_break_dead_config) != HAL_OK) {
+	if(HAL_TIMEx_MasterConfigSynchronization(&pwm_timer3, &pwm_master_config) != HAL_OK) {
 		//Error_Handler();
 	}
 
 	TIM_OC_InitTypeDef pwm_oc_config = {
 		.OCMode = TIM_OCMODE_PWM1,
-		.Pulse = 2000 - 1,
+		.Pulse = 2 - 1,
 		.OCPolarity = TIM_OCPOLARITY_HIGH,
-		.OCNPolarity = TIM_OCNPOLARITY_HIGH,
-		.OCFastMode = TIM_OCFAST_DISABLE,
-		.OCIdleState = TIM_OCIDLESTATE_RESET,
-		.OCNIdleState = TIM_OCNIDLESTATE_RESET,
+		.OCFastMode = TIM_OCFAST_ENABLE
 	};
 
-	if(HAL_TIM_PWM_ConfigChannel(&pwm_timer8, &pwm_oc_config, TIM_CHANNEL_2) != HAL_OK) {
+	if(HAL_TIM_PWM_ConfigChannel(&pwm_timer3, &pwm_oc_config, TIM_CHANNEL_2) != HAL_OK) {
 		//Error_Handler();
 	}
 
-	HAL_TIM_PWM_Start(&pwm_timer8, TIM_CHANNEL_2);
+	HAL_TIM_PWM_Start(&pwm_timer3, TIM_CHANNEL_2);
 }
