31eb7809f61d44e88772137f46be6da1711f3907
Shengwen Cheng <shengwen1997.tw@gmail.com>
2017-04-05 12:10:02 +0800

    Read lidar status before sending command

--- program/driver/device/lidar.c
+++ program/driver/device/lidar.c
@@ -4,7 +4,19 @@
 
 #include "lidar.h"
 
-uint16_t lidar_read_half_word(uint8_t address)
+const uint8_t lidar_dev_address = 0x62 << 1;
+
+static uint8_t lidar_read_byte(uint8_t address)
+{
+        uint8_t result = 0;
+
+        i2c2_write(LIDAR_DEV_ADDRESS, &address, 1);
+        i2c2_read(LIDAR_DEV_ADDRESS, &result, 1);
+
+	return result;
+}
+
+static uint16_t lidar_read_half_word(uint8_t address)
 {
         uint16_t buffer = 0;
 
@@ -13,8 +25,6 @@ uint16_t lidar_read_half_word(uint8_t address)
 
         //convert received data from big endian to little endian
         uint16_t result = buffer >> 8 | buffer << 8;
-
-	
 
 	return result;
 }
@@ -27,7 +37,21 @@ void lidar_write_byte(uint8_t address, uint8_t data)
 
 uint16_t lidar_read_distance(void)
 {
-	lidar_write_byte(LIDAR_ACQ_COMMAND, 0x00);
+	uint8_t lidar_busy_flag = 1; //device busy
+	uint16_t trial = 65535;
+
+	/* wait until lidar is not busy */
+	while(trial-- || lidar_busy_flag) {
+		//read lidar status in byte and apply bit mask to get bit 0 only
+		lidar_busy_flag = lidar_read_byte(LIDAR_STATUS) & 0x01;
+	}
+
+	if(trial == 0) {
+		//while(1); //XXX:test only
+	}
+
+	//send distance measurement command
+	lidar_write_byte(LIDAR_ACQ_COMMAND, 0x03);
 
 	uint16_t distance; //unit: cm
 	distance = lidar_read_half_word(0x8f);
--- program/driver/device/lidar.h
+++ program/driver/device/lidar.h
@@ -1,9 +1,10 @@
 #ifndef __LIDAR_H__
 #define __LIDAR_H__
 
-#define LIDAR_DEV_ADDRESS 0xc4
+#define LIDAR_DEV_ADDRESS (0x62 << 1)
 
 #define LIDAR_ACQ_COMMAND 0x00
+#define LIDAR_STATUS      0x01
 
 uint16_t lidar_read_distance(void);
 
