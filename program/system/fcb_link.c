e95d7b61c73f8b29e936c4a05c75d948679aa344
Shengwen Cheng <shengwen1997.tw@gmail.com>
2017-05-20 00:20:35 +0800

    Create flight control link data queue

--- program/system/fcb_link.c
+++ program/system/fcb_link.c
@@ -3,15 +3,34 @@
 
 #include "FreeRTOS.h"
 #include "task.h"
+#include "semphr.h"
+#include "queue.h"
 
 #include "uart.h"
+
+#include "fcb_link.h"
 
 #include "delay.h"
 
 extern uint16_t lidar_distance;
 
+QueueHandle_t fcb_link_queue_handle;
+
+void send_flow_to_fcb(uint16_t _lidar_distance, float _flow_vx, float _flow_vy)
+{
+	fcb_data_t fcb_data = {
+		.lidar_distance = _lidar_distance,
+		.flow_vx = _flow_vx,
+		.flow_vy = _flow_vy
+	};
+
+	xQueueSendToBack(fcb_link_queue_handle, &fcb_data, 0);
+}
+
 void flight_ctrl_board_link_task(void)
 {
+	fcb_link_queue_handle = xQueueCreate(16, sizeof(fcb_link_queue_handle));
+
 	char buffer[256] = {'\0'};
 
 	while(1) {
--- program/system/fcb_link.h
+++ program/system/fcb_link.h
@@ -1,6 +1,13 @@
 #ifndef __FCB_LINK_H__
 #define __FCB_LINK_H__
 
+typedef struct {
+	uint16_t lidar_distance;
+
+	float flow_vx;
+	float flow_vy;
+} fcb_data_t; 
+
 void flight_ctrl_board_link_task(void);
 
 #endif
