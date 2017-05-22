ac276cb2f7c22f9223a86cb6ad72fc10ee87f4ff
Shengwen Cheng <shengwen1997.tw@gmail.com>
2017-05-22 21:14:38 +0800

    Finish SSD in assembly

--- program/system/ssd16.h
+++ program/system/ssd16.h
@@ -2,8 +2,8 @@
 #define __SSD16_H__
 
 #define simd_square_diff(offset) \
-	 "ldr r4, [%[frame1], # ## offset]\n" \
-	 "ldr r5, [%[frame2], # ## offset]\n" \
+	 "ldr r4, [%[frame1], " #offset "]\n" \
+	 "ldr r5, [%[frame2], " #offset "]\n" \
 	 "ssub16 r4, r4, r5\n"       \
 	 "smlald %[acc_1], %[acc_2], r4, r4\n"
 
@@ -14,16 +14,40 @@
 	 "mov %[acc_1], $0\n" \
 	 "mov %[acc_2], $1\n" \
 	 \
-	 "ldr r4, [%[frame1], #0]\n" \
-	 "ldr r5, [%[frame2], #0]\n" \
-	 "ssub16 r4, r4, r5\n"       \
-	 "smlald %[acc_1], %[acc_2], r4, r4\n" \
+	 simd_square_diff(0) \
+	 simd_square_diff(2) \
+	 simd_square_diff(4) \
+	 simd_square_diff(6) \
 	 \
-	 "ldr r4, [%[frame1], #4]\n" \
-	 "ldr r5, [%[frame2], #4]\n" \
-	 "ssub16 r4, r4, r5\n"       \
-	 "smlald %[acc_1], %[acc_2], r4, r4\n" \
+	 simd_square_diff(40) \
+	 simd_square_diff(42) \
+	 simd_square_diff(44) \
+	 simd_square_diff(46) \
 	 \
+	 simd_square_diff(80) \
+	 simd_square_diff(82) \
+	 simd_square_diff(84) \
+	 simd_square_diff(86) \
+	 \
+	 simd_square_diff(120) \
+	 simd_square_diff(122) \
+	 simd_square_diff(124) \
+	 simd_square_diff(126) \
+	 \
+	 simd_square_diff(160) \
+	 simd_square_diff(162) \
+	 simd_square_diff(164) \
+	 simd_square_diff(166) \
+	 \
+	 simd_square_diff(200) \
+	 simd_square_diff(202) \
+	 simd_square_diff(204) \
+	 simd_square_diff(206) \
+	 \
+	 simd_square_diff(240) \
+	 simd_square_diff(242) \
+	 simd_square_diff(244) \
+	 simd_square_diff(246) \
 	 : [acc_1] "=r" (acc_32[0]), [acc_2] "=r" (acc_32[1]) \
 	 : [frame1] "r" (previos_frame), [frame2] "r" (current_frame) \
 	 : "r4", "r5"             \
