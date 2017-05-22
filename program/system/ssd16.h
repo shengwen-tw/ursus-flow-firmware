dfed3c0ae89bdf5fd32432b172766d825fec72df
Shengwen Cheng <shengwen1997.tw@gmail.com>
2017-05-22 17:58:38 +0800

    SSD16, parrellel multiplacation

--- program/system/ssd16.h
+++ program/system/ssd16.h
@@ -2,18 +2,21 @@
 #define __SSD16_H__
 
 #define calculate_ssd16(previos_frame, current_frame) \
-	({                   \
-	uint64_t acc_64 = 1; \
-	asm volatile(        \
+	({                     \
+	int32_t acc_32[2] = {0, 1};   \
+	asm volatile(          \
+	 "nop\n" \
+	 "mov %[acc_1], $0\n" \
+	 "mov %[acc_2], $1\n" \
 	 "ldr r4, [%[frame1], #0]\n" \
 	 "ldr r5, [%[frame2], #0]\n" \
 	 "ssub16 r4, r4, r5\n"       \
-	 "smlald %[acc_64], r4, r4, %[acc_64]\n" \
-	 : [acc_64] "=r" (acc_64) \
+	 "smlald %[acc_1], %[acc_2], r4, r4\n" \
+	 : [acc_1] "=r" (acc_32[0]), [acc_2] "=r" (acc_32[1]) \
 	 : [frame1] "r" (previos_frame), [frame2] "r" (current_frame) \
 	 : "r4", "r5"             \
 	);                        \
-	((uint32_t *)&acc_64)[0] + ((uint32_t *)&acc_64)[1];    \
+	acc_32[0] + acc_32[1];    \
 	})
 
 #endif
