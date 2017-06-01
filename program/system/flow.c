28ccf9af377df3c3ce7ca9ff3ad6aed2e4052bd6
Shengwen Cheng <shengwen1997.tw@gmail.com>
2017-06-01 22:27:57 +0800

    Port new optical flow

--- program/system/flow.c
+++ program/system/flow.c
@@ -23,7 +23,7 @@
 #include "fcb_link.h"
 #include "system_time.h"
 #include "distance_weighting.h"
-#include "ssd16.h"
+//#include "ssd16.h"
 
 extern TaskHandle_t fcb_link_task_handle;
 extern TaskHandle_t usb_link_task_handle;
@@ -41,50 +41,234 @@ float drift_x = 0;
 float drift_y = 0;
 float drift_z = 0;
 
-/* Find the matching point on two images in local -4 ~ +4 pixels */
-void match_point_local_area(uint16_t *previous_image, uint16_t *current_image,
-                            int8_t *match_x, int8_t *match_y)
+uint32_t calculate_ssd16_row(uint16_t *template_image, uint16_t *search_image, int row_offset)
 {
-	int8_t sd_min_x = -4, sd_min_y = -4;
-	uint32_t sd_min_value = UINT32_MAX;
-	uint32_t current_sd;
+	uint16_t *_template = template_image;
+	uint16_t *_search = search_image;
 
-	uint16_t *current_image_run = current_image;
+	uint32_t ssd = 0;
+	int16_t diff;
 
-	int8_t x, y;
-	for(x = -4; x <= +4; x++) {
-		current_image_run = &current_image[FLOW_IMG_SIZE * x];
+	_template += row_offset * FLOW_IMG_SIZE;
+	_search += row_offset * FLOW_IMG_SIZE;
 
-		for(y = -4; y <= +4; y++) {
-			current_sd = calculate_ssd16(&previous_image[0], &current_image_run[y]);
+	int r;
+	for(r = 0; r < TEMPLATE_SIZE; r++) {
+		diff = *_template - *_search;
+		ssd += diff * diff;
+
+		_template++;
+		_search++;
+	}
+
+	return ssd;
+}
+
+uint32_t calculate_ssd16_column(uint16_t *template_image, uint16_t *search_image, int column_offset)
+{
+	uint16_t *_template = template_image;
+	uint16_t *_search = search_image;
+
+	uint32_t ssd = 0;
+	int16_t diff;
+
+	_template += column_offset;
+	_search += column_offset;
+
+	int c;
+	for(c = 0; c < TEMPLATE_SIZE; c++) {
+		diff = *_template - *_search;
+		ssd += diff * diff;
+
+		_template += FLOW_IMG_SIZE;
+		_search += FLOW_IMG_SIZE;
+	}
+
+	return ssd;
+}
+
+uint32_t calculate_ssd16_full(uint16_t *template_image, uint16_t *search_image)
+{
+	/* ssd minimum value is 1 since later will do the distance weighting
+	   and required not to be 0 */
+	uint32_t ssd = 1;
+
+	uint16_t *_template = template_image;
+	uint16_t *_search = search_image;
+
+	int r, c;
+	for(r = 0; r < TEMPLATE_SIZE; r++) {
+		for(c = 0; c < TEMPLATE_SIZE; c++) {
+			int16_t diff = _template[c] - _search[c];
+
+			ssd += diff * diff;
+		}
+
+		/* point to next row */
+		_template += FLOW_IMG_SIZE;
+		_search += FLOW_IMG_SIZE;
+	}
+
+	return ssd;
+}
+
+/* Find the matching point on two images in local -4 ~ +4 pixels */
+void match_point_local_area_full(uint16_t *previous_image, uint16_t *current_image,
+                                 int8_t *match_x, int8_t *match_y)
+{
+	int8_t ssd_min_x = -4, ssd_min_y = -4;
+	uint32_t ssd_min_value = UINT32_MAX;
+	uint32_t ssd;
+	uint32_t dw_ssd;
+
+	uint16_t *running_search_image = &current_image[0];
+	uint16_t *fixed_template_image = &previous_image[0];
+
+	int8_t r, c;
+	for(r = -4; r <= +4; r++) {
+		running_search_image = &current_image[FLOW_IMG_SIZE * r];
+
+		for(c = -4; c <= +4; c++) {
+			ssd = calculate_ssd16_full(fixed_template_image, &running_search_image[c]);
+
+			flow.subarea_ssd_row_start[r + 4][c + 4] = ssd;
+			flow.subarea_ssd_last[r + 4][c + 4] = ssd;
 
 			/* distance weighting */
-			current_sd *= distance_weighting_table[x + 4][y + 4];
+			dw_ssd = ssd * distance_weighting_table[r + 4][c + 4];
 
-			if(current_sd < sd_min_value) {
-				sd_min_x = x;
-				sd_min_y = y;
-				sd_min_value = current_sd;
+			if(dw_ssd < ssd_min_value) {
+				ssd_min_x = r;
+				ssd_min_y = c;
+				ssd_min_value = dw_ssd;
 			}
 		}
 	}
 
+#if 0
 	/* bad result */
-//	if(sad_min_value > BLOCK_MATCHING_THRESHOLD) {
-//		*match_x = 0;
-//		*match_y = 0;
-//
+	if(ssd_min_value > BLOCK_MATCHING_THRESHOLD) {
+		*match_x = 0;
+		*match_y = 0;
+	}
+#endif
 
-	*match_x = sd_min_x;
-	*match_y = sd_min_y;
+	*match_x = ssd_min_x;
+	*match_y = ssd_min_y;
+}
+
+void match_point_local_area_row_dp(uint16_t *previous_image, uint16_t *current_image,
+                                   int8_t *match_x, int8_t *match_y)
+{
+	int8_t ssd_min_x = -4, ssd_min_y = -4;
+	uint32_t ssd_min_value = UINT32_MAX;
+	uint32_t ssd;
+	uint32_t dw_ssd;
+
+	uint16_t *running_search_image = &current_image[0];
+	uint16_t *fixed_template_image = &previous_image[0];
+
+	uint32_t row_cuttoff_ssd;
+	uint32_t row_addin_ssd;
+
+	int8_t r, c;
+	for(r = -4; r <= +4; r++) {
+		running_search_image = &current_image[FLOW_IMG_SIZE * r];
+
+		for(c = -4; c <= +4; c++) {
+			row_cuttoff_ssd = calculate_ssd16_row(fixed_template_image, &running_search_image[c], -1);
+			row_addin_ssd = calculate_ssd16_row(fixed_template_image, &running_search_image[c], 7);
+
+			ssd = flow.subarea_ssd_row_start[r + 4][c + 4];
+			//cut left old edge and add right new edge
+			ssd -= row_cuttoff_ssd;
+			ssd += row_addin_ssd;
+			//update ssd table
+			flow.subarea_ssd_row_start[r + 4][c + 4] = ssd;
+			flow.subarea_ssd_last[r + 4][c + 4] = ssd;
+
+			//distance weighting
+			dw_ssd = ssd * distance_weighting_table[r + 4][c + 4];
+
+			if(dw_ssd < ssd_min_value) {
+				ssd_min_x = r;
+				ssd_min_y = c;
+				ssd_min_value = dw_ssd;
+			}
+		}
+	}
+
+#if 0
+	/* bad result */
+	if(ssd_min_value > BLOCK_MATCHING_THRESHOLD) {
+		*match_x = 0;
+		*match_y = 0;
+	}
+#endif
+
+	*match_x = ssd_min_x;
+	*match_y = ssd_min_y;
+}
+
+void match_point_local_area_column_dp(uint16_t *previous_image, uint16_t *current_image,
+                                      int8_t *match_x, int8_t *match_y)
+{
+	int8_t ssd_min_x = -4, ssd_min_y = -4;
+	uint32_t ssd_min_value = UINT32_MAX;
+	uint32_t ssd;
+	uint32_t dw_ssd;
+
+	uint16_t *running_search_image = &current_image[0];
+	uint16_t *fixed_template_image = &previous_image[0];
+
+	uint32_t column_cuttoff_ssd;
+	uint32_t column_addin_ssd;
+
+	int8_t r, c;
+	for(r = -4; r <= +4; r++) {
+		running_search_image = &current_image[FLOW_IMG_SIZE * r];
+
+		for(c = -4; c <= +4; c++) {
+			column_cuttoff_ssd = calculate_ssd16_column(fixed_template_image, &running_search_image[c], -1);
+			column_addin_ssd = calculate_ssd16_column(fixed_template_image, &running_search_image[c], 7);
+
+			//read last ssd
+			ssd = flow.subarea_ssd_last[r + 4][c + 4];
+			//cut left old edge and add right new edge
+			ssd -= column_cuttoff_ssd;
+			ssd += column_addin_ssd;
+			//update ssd table
+			flow.subarea_ssd_last[r + 4][c + 4] = ssd;
+
+			//distance weighting
+			dw_ssd = ssd * distance_weighting_table[r + 4][c + 4];
+
+			if(dw_ssd < ssd_min_value) {
+				ssd_min_x = r;
+				ssd_min_y = c;
+				ssd_min_value = dw_ssd;
+			}
+		}
+	}
+
+#if 0
+	/* bad result */
+	if(ssd_min_value > BLOCK_MATCHING_THRESHOLD) {
+		*match_x = 0;
+		*match_y = 0;
+
+	}
+#endif
+
+	*match_x = ssd_min_x;
+	*match_y = ssd_min_y;
 }
 
 void flow_estimate(uint16_t *previous_image, uint16_t *current_image,
                    float *flow_vx, float *flow_vy, float delta_t)
 {
-
-	/* convert the 72x72 start address into 64x64 address */
-	const int offset = TEMPLATE_MIDPOINT_OFFSET + TEMPLATE_SEARCH_SUBAREA_OFFSET;
+	/* convert the 40x40 start address into 32*32 address */
+	int offset = EDGE_PRESERVE_SIZE;
 	int start_x, start_y;
 	uint16_t *frame1;
 	uint16_t *frame2;
@@ -95,35 +279,54 @@ void flow_estimate(uint16_t *previous_image, uint16_t *current_image,
 	//x, y displacement in range of -4 ~ +4 (9 possibilities)
 	uint16_t histogram_x[FLOW_DISP_SIZE] = {0};
 	uint16_t histogram_y[FLOW_DISP_SIZE] = {0};
+	int vote_x, vote_y;
 	int vote_count = 0;
 
 	float predict_disp_x = 0, predict_disp_y = 0;
 
-	uint16_t *frame1_base;
-	uint16_t *frame2_base;
+	/* estimate the first flow by calculating all ssd */
+	start_x = 0 + offset;
+	start_y = 0 + offset;
+	frame1 = &previous_image[start_x * FLOW_IMG_SIZE + start_y];
+	frame2 = &current_image[start_x * FLOW_IMG_SIZE + start_y];
+	match_point_local_area_full(frame1, frame2, &match_x, &match_y);
 
-	/* calculate the flow for 40x40 points using ssd */
-	int x, y;
-	for(x = 0; x < FLOW_COUNT; x++) {
-		start_x = x + offset;
+	flow.match_x[0][0] = match_x + (FLOW_MIDPOINT_OFFSET + 0);
+	flow.match_y[0][0] = match_y + (FLOW_MIDPOINT_OFFSET + 0);
 
-		frame1_base = &previous_image[start_x * FLOW_IMG_SIZE];
-		frame2_base = &current_image[start_x * FLOW_IMG_SIZE];
+	//if not both equal zero
+	if(match_x - match_y) {
+		/* histogram voting */
+		vote_x =  match_x + 4;
+		vote_y =  match_y + 4;
+		histogram_x[vote_x]++;
+		histogram_y[vote_y]++;
 
-		for(y = 0; y < FLOW_COUNT; y++) {
-			start_y = y + offset;
+		predict_disp_x += match_x;
+		predict_disp_y += match_y;
 
-			frame1 = frame1_base + start_y; //locate to previous_image[start_x][start_y]
-			frame2 = frame2_base + start_y; //locate to current_image[start_x][start_y]
-			match_point_local_area(frame1, frame2, &match_x, &match_y);
+		vote_count++;
+	}
 
-			if(match_x == 0 && match_y == 0) {
-				continue;
-			}
+	/* row iteration for first row */
+	volatile int x, y; //XXX:aggressive optimization warning
+	for(y = 1; y < FLOW_COUNT; y++) {
+		start_x = 0 + offset;
+		start_y = y + offset;
+		frame1 = &previous_image[start_x * FLOW_IMG_SIZE + start_y];
+		frame2 = &current_image[start_x * FLOW_IMG_SIZE + start_y];
 
+		match_point_local_area_column_dp(frame1, frame2, &match_x, &match_y);
+
+		/* convert the position relative the full image */
+		flow.match_x[0][y] = match_x + (FLOW_MIDPOINT_OFFSET + 0);
+		flow.match_y[0][y] = match_y + (FLOW_MIDPOINT_OFFSET + y);
+
+		//if not both equal zero
+		if(match_x - match_y) {
 			/* histogram voting */
-			int vote_x =  match_x + 4;
-			int vote_y =  match_y + 4;
+			vote_x =  match_x + 4;
+			vote_y =  match_y + 4;
 			histogram_x[vote_x]++;
 			histogram_y[vote_y]++;
 
@@ -134,11 +337,68 @@ void flow_estimate(uint16_t *previous_image, uint16_t *current_image,
 		}
 	}
 
+	/* estimate the flow by only calculate the non-overlap region's ssd (start from second row) */
+	for(x = 1; x < FLOW_COUNT; x++) {
+		/* row iteration (go down) */
+		start_x = x + offset;
+		start_y = 0 + offset;
+		frame1 = &previous_image[start_x * FLOW_IMG_SIZE + start_y];
+		frame2 = &current_image[start_x * FLOW_IMG_SIZE + start_y];
+
+		match_point_local_area_row_dp(frame1, frame2, &match_x, &match_y);
+
+		/* convert the position relative the full image */
+		flow.match_x[x][0] = match_x + (FLOW_MIDPOINT_OFFSET + x);
+		flow.match_y[x][0] = match_y + (FLOW_MIDPOINT_OFFSET + 0);
+
+		//if not both equal zero
+		if(match_x - match_y) {
+			/* histogram voting */
+			vote_x =  match_x + 4;
+			vote_y =  match_y + 4;
+			histogram_x[vote_x]++;
+			histogram_y[vote_y]++;
+
+			predict_disp_x += match_x;
+			predict_disp_y += match_y;
+
+			vote_count++;
+		}
+
+		/* column iteration (go right) */
+		for(y = 1; y < FLOW_COUNT; y++) {
+			start_x = x + offset;
+			start_y = y + offset;
+			frame1 = &previous_image[start_x * FLOW_IMG_SIZE + start_y];
+			frame2 = &current_image[start_x * FLOW_IMG_SIZE + start_y];
+
+			match_point_local_area_column_dp(frame1, frame2, &match_x, &match_y);
+
+			/* convert the position relative the full image */
+			flow.match_x[x][y] = match_x + (FLOW_MIDPOINT_OFFSET + x);
+			flow.match_y[x][y] = match_y + (FLOW_MIDPOINT_OFFSET + y);
+
+			//if not both equal zero
+			if(match_x - match_y) {
+				/* histogram voting */
+				vote_x =  match_x + 4;
+				vote_y =  match_y + 4;
+				histogram_x[vote_x]++;
+				histogram_y[vote_y]++;
+
+				predict_disp_x += match_x;
+				predict_disp_y += match_y;
+
+				vote_count++;
+			}
+		}
+	}
+
 	if(vote_count < HISTOGRAM_THRESHOLD) {
 		*flow_vx = 0;
 		*flow_vy = 0;
 
-		gpio_off(LED_2);
+		gpio_off(LED_2); //no flow
 
 		return;
 	}
@@ -150,7 +410,7 @@ void flow_estimate(uint16_t *previous_image, uint16_t *current_image,
 	*flow_vx = +((float)lidar_distance / FOCAL_LENGTH_PX * predict_disp_x) / delta_t;
 	*flow_vy = -((float)lidar_distance / FOCAL_LENGTH_PX * predict_disp_y) / delta_t;
 
-	gpio_on(LED_2);
+	gpio_on(LED_2); //flow detected
 }
 
 void flow_estimate_task(void)
--- program/system/flow.h
+++ program/system/flow.h
@@ -10,15 +10,20 @@
 #define M               166.67 //[# of pixels / mm]
 #define RETINA_SIZE     0.006f //[mm], retina size of mt9v034 is 6um
 
-#define FLOW_COUNT          32 //number of optical flow point to be calculated
+#define FLOW_COUNT          64 //number of optical flow point to be calculated
 #define TEMPLATE_SIZE       8  //sum of absolute difference template size
 #define SEARCH_SUBAREA_SIZE 16 //only match the template in near -4 ~ +4 range
+#define SUBAREA_SSD_SIZE (SEARCH_SUBAREA_SIZE - TEMPLATE_SIZE + 1) //9x9
+
+#define EDGE_PRESERVE_SIZE  4  //the offset from origin to middlemost template in the first subarea
 
 #define TEMPLATE_MIDPOINT_OFFSET        3 //distance from template edge to its midpoint (8x8 template)
 #define TEMPLATE_SEARCH_SUBAREA_OFFSET  3 //distance from search subarea edge to the middlemost template
+//the offset from origin to first flow point
+#define FLOW_MIDPOINT_OFFSET (TEMPLATE_MIDPOINT_OFFSET + TEMPLATE_SEARCH_SUBAREA_OFFSET)
 
-#define BLOCK_MATCHING_THRESHOLD 1024 //25% of max difference (16-bits number)
-#define HISTOGRAM_THRESHOLD 256 //25% of 1024 (32*32)
+//#define BLOCK_MATCHING_THRESHOLD 1024 //25% of max difference (16-bits number)
+#define HISTOGRAM_THRESHOLD 409 //64x64 = 4096
 
 /* since we only search the near -4 ~ +4 pixels for flow,
    there are only 9x9 matching possibilites */
@@ -26,13 +31,19 @@
 
 typedef struct {
 	uint16_t frame[FLOW_IMG_SIZE][FLOW_IMG_SIZE];
+	uint8_t match_point[FLOW_IMG_SIZE][FLOW_IMG_SIZE];
 } image_t;
 
 typedef struct {
+	uint32_t subarea_ssd_row_start[SUBAREA_SSD_SIZE][SUBAREA_SSD_SIZE];
+	uint32_t subarea_ssd_last[SUBAREA_SSD_SIZE][SUBAREA_SSD_SIZE];
+
+	image_t image[2];
+
 	uint8_t match_x[FLOW_IMG_SIZE][FLOW_IMG_SIZE];
 	uint8_t match_y[FLOW_IMG_SIZE][FLOW_IMG_SIZE];
 
-	image_t image[2];
+	uint8_t distance[FLOW_IMG_SIZE][FLOW_IMG_SIZE];
 } flow_t;
 
 void flow_estimate_task(void);
