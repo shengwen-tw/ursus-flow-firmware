55f356fe619ee5cd30574dfb7feafdc95cd08c26
Shengwen Cheng <shengwen1997.tw@gmail.com>
2017-06-03 20:41:22 +0800

    Able to receive header message

--- tools/ros_ws/src/usb_monitor/main.cpp
+++ tools/ros_ws/src/usb_monitor/main.cpp
@@ -1,16 +1,73 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/time.h>
+
 #include <usb.h>
 #include <libusb-1.0/libusb.h>
+#include "opencv2/opencv.hpp"
+#include <ros/ros.h>
+#include <std_msgs/Float32.h>
+#include <cv_bridge/cv_bridge.h>
+#include <tf/transform_broadcaster.h>
 
 #define VENDOR_ID  0x0483
 #define PRODUCT_ID 0x5740
+
+#define MAX_IMAGE_SIZE (188 * 120)
+#define BUFFER_SIZE (MAX_IMAGE_SIZE + 512)
+
+#define PACKET_HEADER_SIZE 20
 
 /* Use lsusb -v to find the correspond values */
 static int ep_in_address  = 0x81;
 static int ep_out_address = 0x01;
 
 static struct libusb_device_handle *dev_handle = NULL;
+
+cv::Mat cv_image;
+int image_width = 40;  //Default
+int image_height = 40; //Default
+
+/* on-board info */
+uint16_t lidar_distance = 0;
+float gyro_x = 0, gyro_y = 0, gyro_z = 0;
+uint8_t gyro_calib_enable = 0;
+
+bool _usb_init()
+{
+	int rc;
+
+	/* initialize libusb */
+	rc = libusb_init(NULL);
+	if (rc < 0) {
+		printf("Failed to initialize libusb!\n");
+		return false;
+	}
+
+	libusb_set_debug(NULL, 3);
+
+	/* open device to get the token */
+	dev_handle = libusb_open_device_with_vid_pid(NULL, VENDOR_ID, PRODUCT_ID);
+	if(!dev_handle) {
+		printf("error: device not found (please check vendor id and product id).\n");
+		return false;
+	}
+
+	/* claiming interface */
+	for(int if_num = 0; if_num < 2; if_num++) {
+		if (libusb_kernel_driver_active(dev_handle, if_num)) {
+			libusb_detach_kernel_driver(dev_handle, if_num);
+		}
+
+		rc = libusb_claim_interface(dev_handle, if_num);
+		if (rc < 0) {
+			printf("error: failed to claim the usb interface.\n");
+			return false;
+		}
+	}
+
+	return true;
+}
 
 int usb_read(uint8_t *buffer, int size, int timeout)
 {
@@ -29,54 +86,132 @@ int usb_read(uint8_t *buffer, int size, int timeout)
 	return received_len;
 }
 
-int main()
+bool usb_receive_onboard_info(uint16_t *buffer)
 {
-	int rc;
-
-	/* initialize libusb */
-	rc = libusb_init(NULL);
-	if (rc < 0) {
-		printf("Failed to initialize libusb!\n");
-		return 0;
-	}
-
-	libusb_set_debug(NULL, 3);
-
-	/* open device to get the token */
-	dev_handle = libusb_open_device_with_vid_pid(NULL, VENDOR_ID, PRODUCT_ID);
-	if(!dev_handle) {
-		printf("error: device not found (please check vendor id and product id).\n");
-		return 0;
-	}
-
-	/* claiming interface */
-	for(int if_num = 0; if_num < 2; if_num++) {
-		if (libusb_kernel_driver_active(dev_handle, if_num)) {
-			libusb_detach_kernel_driver(dev_handle, if_num);
-		}
-
-		rc = libusb_claim_interface(dev_handle, if_num);
-		if (rc < 0) {
-			printf("error: failed to claim the usb interface.\n");
-			return false;
-		}
-	}
-
-	/* receive data from usb */
-	char buffer[10000] = {'\0'};
+	int received_len;
+	int size_to_receive = 6400;
+ 
+	/* wait for header message */
 	while(1) {
-		int received_len = usb_read((uint8_t *)buffer, 6400, 1000);
-		if(received_len > 0) {
-			printf("%c %d\n", buffer[0], received_len);
-		} else if(received_len == 0) {
-			printf("usb reception timeout.\n");	
-		} else {
-			printf("\n[usb disconnected]\n");
-			return 0;
+		received_len = usb_read((uint8_t *)buffer, size_to_receive, 1000);
+
+		if(received_len == 0 || received_len == -1) {
+			return false;
+		} else if(received_len == PACKET_HEADER_SIZE) {
+			break;
 		}
+
 	}
+
+	/* Lidar distance */
+	int append_size = 3;
+
+	//unpack header - lidar distance
+	memcpy(&lidar_distance, (uint8_t *)buffer + append_size, sizeof(uint16_t));
+	append_size += sizeof(uint16_t);
+
+	memcpy(&gyro_calib_enable, (uint8_t *)buffer + append_size, sizeof(uint8_t));
+	append_size += sizeof(uint8_t);
+
+	//unpack header - gyro x
+	memcpy(&gyro_x, (uint8_t *)buffer + append_size, sizeof(float));
+	append_size += sizeof(float);
+
+	//unpack header - gyro y
+	memcpy(&gyro_y, (uint8_t *)buffer + append_size, sizeof(float));
+	append_size += sizeof(float);
+
+	//unpack header -  gyro z
+	memcpy(&gyro_z, (uint8_t *)buffer + append_size, sizeof(float));
+	append_size += sizeof(float);
+
+	//unpack header - image width
+	memcpy(&image_width, (uint8_t *)buffer + append_size, sizeof(uint8_t));
+	append_size += sizeof(uint8_t);
+
+	//unpack header -  image_height
+	memcpy(&image_height, (uint8_t *)buffer + append_size, sizeof(uint8_t));
+	append_size += sizeof(uint8_t);
+
+	/* receive camera image in two parts */
+	//received_len = usb_read((uint8_t *)buffer, size_to_receive, 1000);
+
+	if(received_len > 0 && gyro_calib_enable == 0) {
+		//printf("received new image, size = %d bytes\n", received_len);
+	} else if(received_len == 0 || received_len == -1) {
+		return false;
+	}
+
+	return true;
+}
+
+int main(int argc, char **argv)
+{
+	/* initialize ROS */
+	ros::init(argc, argv, "ursusflow_usb_sim");
+	ros::Time::init();
+
+	ros::NodeHandle node;
+	ros::Publisher ros_image_publisher =
+	        node.advertise<sensor_msgs::Image>("ursusflow_usb/flow_image", 10);
+
+	if(_usb_init() == false) {
+		return 0;
+	}
+
+	uint16_t buffer[10000];
+
+	double current_time = 0;
+	double previous_time = 0;
+	double delta_t = 0;
+
+	while(1) {
+		if(usb_receive_onboard_info(buffer) == false) {
+			printf("[usb disconnected]\n");
+			break;
+		}
+
+		current_time = ros::Time::now().toSec();
+		delta_t = current_time - previous_time; //calculate delta_t
+		previous_time = current_time; //update timer
+
+		if(gyro_calib_enable == 0) {
+			printf("image size: %dx%d\n"
+			       "lidar distance: %dcm\n"
+			       "gyro_x: %+.3f\n"
+			       "gyro_y: %+.3f\n"
+			       "gyro_z: %+.3f\n"
+			       "fps: %f\n"
+			       "delta t: %f\n"
+			       "\033[2J\033[1;1H",
+			       image_width,
+			       image_height,
+			       lidar_distance,
+			       gyro_x,
+			       gyro_y,
+			       gyro_z,
+			       1.0f / delta_t,
+			       delta_t
+			      );
+		} else {
+			printf("[gyroscope bias calibration]\n"
+			       "bias x: %+.3f\n"
+			       "bias y: %+.3f\n"
+			       "bias z: %+.3f\n"
+			       "\033[2J\033[1;1H",
+			       gyro_x,
+			       gyro_y,
+			       gyro_z);
+		}
+
+		//cv::imshow("ursus-flow camera", cv_image);
+		//cv::waitKey(1);
+	}
+
+	printf("[leave]\n");
 
 	/* close and exit */
+	cv_image.release();
 	libusb_close(dev_handle);
 	libusb_exit(NULL);
 }
