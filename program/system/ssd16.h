47257300177eb3671eb2f58626e45bf206d71bba
Shengwen Cheng <shengwen1997.tw@gmail.com>
2017-06-14 23:21:37 +0800

    Fix ssd image offset calculation

--- program/system/ssd16.h
+++ program/system/ssd16.h
@@ -1,9 +1,15 @@
 #ifndef __SSD16_H__
 #define __SSD16_H__
 
-#define simd_square_diff(offset) \
-	 "ldr r4, [%[frame1], " #offset "]\n" \
-	 "ldr r5, [%[frame2], " #offset "]\n" \
+/* convet image (r, c) position to image array offset in byte
+ * input = row, column
+ * output = (row * sizeof(uint16_t) * image_size) + (column * sizeof(uint16_t)) */
+#define img_offset_calc(r, c) \
+	"((" #r "* 2) * 40) + (" #c "* 2)" //((r * 2) * 40) + (c * 2)
+
+#define simd_square_diff(r, c) \
+	 "ldr r4, [%[frame1], " img_offset_calc(r, c) "]\n" \
+	 "ldr r5, [%[frame2], " img_offset_calc(r, c) "]\n" \
 	 "ssub16 r4, r4, r5\n"       \
 	 "smlald r6, r7, r4, r4\n"
 
@@ -14,45 +20,45 @@
 	 "mov r6, $0\n"  \
 	 "mov r7, $1\n"  \
 	 \
-	 simd_square_diff(0) \
-	 simd_square_diff(2) \
-	 simd_square_diff(4) \
-	 simd_square_diff(6) \
+	 simd_square_diff(0, 0) \
+	 simd_square_diff(0, 2) \
+	 simd_square_diff(0, 4) \
+	 simd_square_diff(0, 8) \
 	 \
-	 simd_square_diff(40) \
-	 simd_square_diff(42) \
-	 simd_square_diff(44) \
-	 simd_square_diff(46) \
+	 simd_square_diff(1, 0) \
+	 simd_square_diff(1, 2) \
+	 simd_square_diff(1, 4) \
+	 simd_square_diff(1, 8) \
 	 \
-	 simd_square_diff(80) \
-	 simd_square_diff(82) \
-	 simd_square_diff(84) \
-	 simd_square_diff(86) \
+	 simd_square_diff(2, 0) \
+	 simd_square_diff(2, 2) \
+	 simd_square_diff(2, 4) \
+	 simd_square_diff(2, 8) \
 	 \
-	 simd_square_diff(120) \
-	 simd_square_diff(122) \
-	 simd_square_diff(124) \
-	 simd_square_diff(126) \
+	 simd_square_diff(3, 0) \
+	 simd_square_diff(3, 2) \
+	 simd_square_diff(3, 4) \
+	 simd_square_diff(3, 8) \
 	 \
-	 simd_square_diff(160) \
-	 simd_square_diff(162) \
-	 simd_square_diff(164) \
-	 simd_square_diff(166) \
+	 simd_square_diff(4, 0) \
+	 simd_square_diff(4, 2) \
+	 simd_square_diff(4, 4) \
+	 simd_square_diff(4, 8) \
 	 \
-	 simd_square_diff(200) \
-	 simd_square_diff(202) \
-	 simd_square_diff(204) \
-	 simd_square_diff(206) \
+	 simd_square_diff(5, 0) \
+	 simd_square_diff(5, 2) \
+	 simd_square_diff(5, 4) \
+	 simd_square_diff(5, 8) \
 	 \
-	 simd_square_diff(240) \
-	 simd_square_diff(242) \
-	 simd_square_diff(244) \
-	 simd_square_diff(246) \
+	 simd_square_diff(6, 0) \
+	 simd_square_diff(6, 2) \
+	 simd_square_diff(6, 4) \
+	 simd_square_diff(6, 8) \
 	 \
-	 simd_square_diff(280) \
-	 simd_square_diff(282) \
-	 simd_square_diff(284) \
-	 simd_square_diff(286) \
+	 simd_square_diff(7, 0) \
+	 simd_square_diff(7, 2) \
+	 simd_square_diff(7, 4) \
+	 simd_square_diff(7, 8) \
 	 \
 	 "add r6, r6, r7\n"    \
 	 "str r6, %[acc]\n"    \
