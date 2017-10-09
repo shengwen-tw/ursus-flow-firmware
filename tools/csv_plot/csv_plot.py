d497ce5525c9fa909ed6a6218c7809a7263c91f0
Shengwen Cheng <shengwen1997.tw@gmail.com>
2017-10-09 16:43:53 +0800

    Plot accelerometer LPF

--- tools/csv_plot/csv_plot.py
+++ tools/csv_plot/csv_plot.py
@@ -1,14 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-csv = np.genfromtxt('flow.csv', delimiter=',')
+csv = np.genfromtxt('accel_log.csv', delimiter=',')
 data_size = len(csv[:, 0])
 
 #prediction control variable u (from accelerometer)
 time = csv[:, 0]
-end_time = time[data_size - 1] - 3.2
-data1 = csv[:, 1] / 100
-data2 = csv[:, 2] / 100
+end_time = time[data_size - 1]
+data1 = csv[:, 1]
+data2 = csv[:, 2]
 
 #plot result
 plt.figure('csv plot tool')
@@ -16,7 +16,7 @@ plt.xlim(0, end_time)
 plt.plot(time, data1)
 plt.plot(time, data2)
 plt.xlabel('time (s)', fontsize=18)
-plt.ylabel('velocity (m/s)', fontsize=18)
-plt.legend(['kalman filter', 'optical flow'], loc='upper left')
+plt.ylabel('acceleration (a/s)', fontsize=18)
+plt.legend(['low-pass filter', 'raw data'], loc='upper left')
 
 plt.show()
